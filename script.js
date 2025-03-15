36aa402a0f6b46b5754d476876af5850c31c3fa0
nublius <184036331+nublius@users.noreply.github.com>
2025-03-15 00:02:44 -0700

    Complete round mechanics

--- script.js
+++ script.js
@@ -40,5 +40,22 @@ function playRound(computerChoice, humanChoice) {
     } else if (computerChoice === "paper" && humanChoice === "rock") {
         console.log("You lose! Paper beats rock.")
         computerScore++;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose! Scissors beat paper.")
+        computerScore++;
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win! Scissors beat paper.");
+        humanScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win! Rock beats scissors.")
+        humanScore++;
     }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComupterChoice();
+
+playRound(humanSelection, computerSelection);
